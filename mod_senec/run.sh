4fdaddda38216ecf23ade0349662ae1c015e0ff1
René Keller <rene.keller@sva.de>
2022-11-22 21:10:10 +0100

    Create run.sh

---
+++ mod_senec/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/with-contenv bashio
+set -e
+
+bashio::log.info "Starting..."
+SENEC_HOST=$(bashio::config 'SENEC_HOST')
+export SENEC_HOST=$(bashio::config 'SENEC_HOST')
+bashio::log.info "SENEC_HOST: $(SENEC_HOST)"
+bashio::log.info "Starting Python Trigger Script..."
+python3 /app/trigger_senec.py
