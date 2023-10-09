a40dfad57d27fe8155e350171b0354449a9428aa
René Keller <rene@familie-keller.info>
2023-10-09 21:40:13 +0200

    Update trigger_senec.py
    fix newest selenium version and https errors.

--- mod_senec/trigger_senec.py
+++ mod_senec/trigger_senec.py
@@ -7,27 +7,25 @@ import sys
 import time
 import os
 
-
-
 chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument('--headless')
+chrome_options.add_argument("--headless=new")
+chrome_options.binary_location = "/usr/bin/chromium-browser"
+chrome_options.add_argument("--ignore-certificate-errors")
 chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument("--disable-setuid-sandbox")
-
 driver = webdriver.Chrome(options=chrome_options)
+
 start_url = "https://" + sys.argv[1]
 print("Start URL: " + start_url)
 driver.get(start_url)
 #print(driver.page_source.encode("utf-8"))
 submenu_setup = driver.find_element(By.XPATH, '//*[@id="lSetup"]')
-time.sleep(1)
+time.sleep(0.5)
 driver.execute_script("arguments[0].click();", submenu_setup)
-time.sleep(1)
+time.sleep(0.5)
 #print(driver.page_source.encode("utf-8"))
 submenu_maintenance = driver.find_element(By.XPATH, '//*[@id="btnKonfiguration_AccuService"]')
 driver.execute_script("arguments[0].click();", submenu_maintenance)
-time.sleep(1)
+time.sleep(0.5)
 driver.find_element(By.ID, "iconSafetyCharge").click()
-time.sleep(1)
+time.sleep(0.5)
 driver.quit()
